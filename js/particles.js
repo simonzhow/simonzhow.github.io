3ebf623db4a02f904a8fb8d34e6d50aed8eda799
Simon Zhou <simon.zhou@ucla.edu>
2017-12-30 07:41:27 -0800

    Mobile friendly-ish

--- js/particles.js
+++ js/particles.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('home_particles'),
+var canvas = document.getElementById('particles'),
    can_w = parseInt(canvas.getAttribute('width')),
    can_h = parseInt(canvas.getAttribute('height')),
    ctx = canvas.getContext('2d');
