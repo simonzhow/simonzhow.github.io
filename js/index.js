6207cb2d64ed4a4667040e3592c8311a75160877
Simon Zhou <simon.zhou@ucla.edu>
2018-01-03 18:03:17 -0800

    Not mobile compliant - desktop version works for projects

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
+// Smooth transition from section to section
 $(".scrolly").click(function() {
+    // About Me
     $('html, body').animate({
-        scrollTop: $("#bio").offset().top
+      scrollTop: $("#bio").offset().top
     }, 750);
+
+    // // Projects
+    // $('html, body').animate({
+    //   scrollTop: $('#projects').offset().scrollTop
+    // }, 750);
+    //
+    //
+    // // Contact
+    // $('html, body').animate({
+    //   scrollTop: $('#contact').offset().scrollTop
+    // }, 750);
 });
