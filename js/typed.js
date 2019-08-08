915246294309425bce660c34e56af07239c631ab
Simon Zhou <simon.zhou@ucla.edu>
2019-08-07 18:25:47 -0700

    Added more details about self

--- js/typed.js
+++ js/typed.js
@@ -1,37 +1,37 @@
-const adjectivesArray = ["box.", "snowboard.", "design.", "create."];
+const adjectivesArray = ["eat.", "box.", "snowboard.", "design."];
 //Cloud Function
 $(document).ready(function() {
-    $(".home-intro_typed-text").typed({
-        strings: adjectivesArray,
-        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-        stringsElement: null,
-        // typing speed
-        typeSpeed: 60,
-        // time before typing starts
-        startDelay: 0,
-        // backspacing speed
-        backSpeed: 50,
-        // time before backspacing
-        backDelay: 700,
-        // loop
-        loop: false,
-        // false = infinite
-        loopCount: false,
-        // show cursor
-        showCursor: true,
-        // character for cursor
-        cursorChar: "|",
-        // attribute to type (null == text)
-        attr: null,
-        // either html or text
-        contentType: 'html',
-        // call when done callback function
-        callback: function () {},
-        // starting callback function before each string
-        preStringTyped: function () {},
-        //callback for every typed string
-        onStringTyped: function () {},
-        // callback for reset
-        resetCallback: function () {}
-    });
+  $(".home-intro_typed-text").typed({
+    strings: adjectivesArray,
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 60,
+    // time before typing starts
+    startDelay: 0,
+    // backspacing speed
+    backSpeed: 50,
+    // time before backspacing
+    backDelay: 700,
+    // loop
+    loop: false,
+    // false = infinite
+    loopCount: false,
+    // show cursor
+    showCursor: true,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: "html",
+    // call when done callback function
+    callback: function() {},
+    // starting callback function before each string
+    preStringTyped: function() {},
+    //callback for every typed string
+    onStringTyped: function() {},
+    // callback for reset
+    resetCallback: function() {}
+  });
 });
